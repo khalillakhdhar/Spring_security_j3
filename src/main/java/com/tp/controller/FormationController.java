80ad11919818c797de269ab40ce72593b843b626
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-23 13:42:16 +0100

    Update FormationController.java

--- src/main/java/com/tp/controller/FormationController.java
+++ src/main/java/com/tp/controller/FormationController.java
@@ -2,8 +2,12 @@ package com.tp.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tp.model.Formation;
@@ -20,6 +24,12 @@ public class FormationController {
 	{
 		return (List<Formation>) formationRepository.findAll();
 		
+		
+	}
+	@PostMapping("formations")
+	public Formation addFormation(@Valid @RequestBody Formation formation)
+	{
+		return formationRepository.save(formation);
 		
 	}
 	
