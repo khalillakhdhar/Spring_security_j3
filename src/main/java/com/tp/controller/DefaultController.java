a565cb22d92cbce81dc08e4a058a7c4905d5feca
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-23 11:57:01 +0100

    crudrepo

--- src/main/java/com/tp/controller/DefaultController.java
+++ src/main/java/com/tp/controller/DefaultController.java
@@ -3,6 +3,7 @@ package com.tp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tp.model.User;
 
@@ -26,5 +27,11 @@ public String datapost()
 return "you are posting data";	
 }
 @GetMapping("home")
+public ModelAndView homepage()
+{
+ModelAndView mv=new ModelAndView("homepage.html");
+return mv;
+
+}
 
 }
